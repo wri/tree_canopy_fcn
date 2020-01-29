4f00cd8c3a4ded8b849ce59ab8b61e522933b3fa
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-01-29 11:15:23 -0800

    scripts.share_dl: unique reader list

--- scripts/share_dl_products.py
+++ scripts/share_dl_products.py
@@ -18,7 +18,7 @@ ULU_PREDS_PRODUCT='wri:ulu_predictions-india'
 def add_readers(prod,readers):
     p=Product.get(prod)
     print(p)
-    p.readers=p.readers+readers
+    p.readers=list(set(p.readers+readers))
     p.save()
     status=p.update_related_objects_permissions(readers=readers)
     return p,status
@@ -32,31 +32,31 @@ print('='*50)
 
 if DEV:
     p,dev_status=add_readers(DEV_PRODUCT,DEV_READERS)
+    print(p.readers)
     if dev_status:
         dev_status.wait_for_completion()
     dev_status
-    print(p.readers)
     print(dev_status)
 else:
     t_p,tree_status=add_readers(TREE_PRODUCT,NGS)
     u_p,ulu_status=add_readers(ULU_PRODUCT,NGS)
     # up_p,ulu_preds_status=add_readers(ULU_PREDS_PRODUCT,NGS)
 
+    print(t_p.readers)
     if tree_status:
         tree_status.wait_for_completion()
-    print(t_p.readers)
     print(tree_status)
-    print('-'*50)
 
+    print('-'*50)
+    print(u_p.readers)
     if ulu_status:
         ulu_status.wait_for_completion()
-    print(u_p.readers)
     print(ulu_status)
-    print('-'*50)
     
+    # print('-'*50)
+    # print(up_p.readers)
     # if ulu_preds_status:
     #     ulu_preds_status.wait_for_completion()
-    # print(up_p.readers)
     # print(ulu_preds_status)
 
 print('='*50)
